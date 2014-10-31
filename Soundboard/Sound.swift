8816ecafb46592379354caf3000bbe76bdf6ebbf
Michael Daleo <mike@novocentpartners.com>
2014-10-31 13:32:00 -0400

    Lecture 32

---
+++ Soundboard/Sound.swift
@@ -0,0 +1,16 @@
+//
+//  Sound.swift
+//  Soundboard
+//
+//  Created by MacBook on 10/31/14.
+//  Copyright (c) 2014 MacBook. All rights reserved.
+//
+
+import Foundation
+
+class Sound{
+    var name = "Meow"
+    var URL = NSURL()
+
+    
+}
--- Soundboard/SoundListViewController.swift
+++ Soundboard/SoundListViewController.swift
@@ -12,22 +12,40 @@ import AVFoundation
 class SoundListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+
     var audioPlayer = AVAudioPlayer()
+    
+    var sounds: [Sound] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // My own code and sniz
         self.tableView.dataSource = self
         self.tableView.delegate = self
+        
+        var soundPath = NSBundle.mainBundle().pathForResource("thumpy", ofType: "m4a")
+        var soundURL = NSURL.fileURLWithPath(soundPath!)
+        
+        var sound1 = Sound()
+        sound1.name = "Sound 1 name"
+        sound1.URL = soundURL!
+        
+        var sound2 = Sound()
+        sound2.name = "Turndown for what"
+        sound2.URL = soundURL!
+        
+        self.sounds.append(sound1)
+        self.sounds.append(sound2)
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.sounds.count
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var sound = self.sounds[indexPath.row]
         var cell = UITableViewCell()
-        cell.textLabel.text = "BRUH"
+        cell.textLabel.text = sound.name
         return cell
     }
     

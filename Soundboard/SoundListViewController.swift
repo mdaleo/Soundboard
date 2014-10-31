1ebcc7e17d87d6121754aea3fbdf428a7a5cd0a6
Michael Daleo <mike@novocentpartners.com>
2014-10-31 09:52:54 -0400

    Initial commit. Lecture 29

---
+++ Soundboard/SoundListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SoundListViewController
+//  Soundboard
+//
+//  Created by MacBook on 10/31/14.
+//  Copyright (c) 2014 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class SoundListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // My own code and sniz
+    }
+
+
+
+}
+

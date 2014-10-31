21630abfbe13104a2d05d5744283f15e6af7b976
Michael Daleo <mike@novocentpartners.com>
2014-10-31 10:13:14 -0400

    Lecture 30

--- Soundboard/SoundListViewController.swift
+++ Soundboard/SoundListViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class SoundListViewController: UIViewController {
+class SoundListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // My own code and sniz
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel.text = "BRUH"
+        return cell
+    }
 
 
 }

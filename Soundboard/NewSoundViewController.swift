af11b450a90ba9cf080cf16b04c7690d58e1ad4a
Michael Daleo <mike@novocentpartners.com>
2014-10-31 15:55:31 -0400

    Lecture 33

---
+++ Soundboard/NewSoundViewController.swift
@@ -0,0 +1,35 @@
+//
+//  New SoundViewController.swift
+//  Soundboard
+//
+//  Created by MacBook on 10/31/14.
+//  Copyright (c) 2014 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class NewSoundViewController : UIViewController {
+	override func viewDidLoad() {
+        super.viewDidLoad()
+        // Tine to swell
+        
+        
+        
+    }
+    
+
+    @IBAction func cancelTapped(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func saveTapped(sender: AnyObject) {
+        // Step one
+        
+        // Step two
+        
+        // Close
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+}
